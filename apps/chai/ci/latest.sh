7173ed43f8cea3cea057b1325749dbb584cdf0cc
Liana <liana@lianas.org>
2024-12-17 11:45:58 -0600

    Merge upstream and develop Chai-1
    Merge upstream and develop Chai-1 to use the same Dockerfile with
    multiple channels

--- apps/chai/ci/latest.sh
+++ apps/chai/ci/latest.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/chaidiscovery/chai-lab/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+channel=$1
+
+if [[ "${channel}" == "stable" ]]; then
+  version=$(curl -sX GET "https://api.github.com/repos/chaidiscovery/chai-lab/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
+  version="${version#*v}"
+  version="${version#*release-}"
+  printf "%s" "${version}"
+fi
+
+if [[ "${channel}" == "develop" ]]; then
+  git clone --quiet https://github.com/amelie-iska/chai-lab.git /tmp/chai
+  pushd /tmp/chai > /dev/null || exit
+  version=$(git rev-list --count --first-parent HEAD)
+  popd > /dev/null || exit
+  rm -rf /tmp/chai
+  printf "%s" "${version}"
+fi
