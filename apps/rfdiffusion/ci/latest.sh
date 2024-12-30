ee9970df302a57796039791681002fc0582cf794
Liana <liana@lianas.org>
2024-12-30 13:49:44 -0600

    Add RFdiffusion

---
+++ apps/rfdiffusion/ci/latest.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ "${channel}" == "stable" ]]; then
+  version=$(curl -sX GET "https://api.github.com/repos/RosettaCommons/RFdiffusion/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
+  version="${version#*v}"
+  version="${version#*release-}"
+  printf "%s" "${version}"
+fi
+
+if [[ "${channel}" == "develop" ]]; then
+  git clone --quiet https://github.com/amelie-iska/RFdiffusion.git /tmp/rfdiffusion
+  pushd /tmp/rfdiffusion > /dev/null || exit
+  version=$(git rev-list --count --first-parent HEAD)
+  popd > /dev/null || exit
+  rm -rf /tmp/rfdiffusion
+  printf "%s" "${version}"
+fi
